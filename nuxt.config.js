81a14ddb0aada9ada92b4fd5c6b1d2936c00d7ef
maiacodes <hi@maia.codes>
2022-05-21 21:23:23 +0100

    added meta tags for social

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'title', content: 'Tokle: Guess the album cover!' },
+      { name: 'description', content: 'The aim of the game? Listen to the audio and guess the cover art of the popular song. Skipped or incorrect attempts unlock more of the audio, answer in as few tries as possible and share your score!' },
+      { name: 'theme-color', content: '#232323' },
+    //  Opengraph
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://tokle.app' },
+      { property: 'og:title', content: 'Tokle: Guess the album cover!' },
+      { property: 'og:description', content: 'The aim of the game? Listen to the audio and guess the cover art of the popular song. Skipped or incorrect attempts unlock more of the audio, answer in as few tries as possible and share your score!' },
+      { property: 'og:image', content: 'https://tokle.app/wordmark.png' },
+    //  Twitter
+      { property: 'twitter:image', content: 'https://tokle.app/logo.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
